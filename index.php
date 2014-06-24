76c394acf34397fe83cca6c3f55eb9a1456a6f73
LukeXF <me@luke.sx>
2014-06-24 04:11:23 +0100

    Added protected directory & css base.

--- index.php
+++ index.php
@@ -21,19 +21,7 @@ echo "PHP Test!";
 ?>
 
 <?php
-// Creates connection to database
-$con = mysqli_connect(
-/* Host address */	"localhost",
-/* Username     */	"ChromaDonateUser",
-/* Password     */	"CWbcfBGfuuq6RTuW",
-/* DB Name      */	"ChromaDonate");
-
-// Check connection of database
-if (mysqli_connect_errno()) {
-  echo "<br>Failed to connect to MySQL: <br><b>" . mysqli_connect_error() . "</b>";
-}
-
-
+	require 'protected/config.php';
 ?>
 
 </body>
