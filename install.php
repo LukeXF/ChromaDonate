7c296891b87d543eabdadd4db89577d96a787bd0
LukeXF <me@luke.sx>
2014-06-24 01:16:37 +0100

    Added meta data + Split up installation

--- install.php
+++ install.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 
-<h1>ChromaDonate</h1>
+<h1>ChromaDonate - Install</h1>
 
 <?php
 echo "PHP Test!";
@@ -33,6 +33,27 @@ if (mysqli_connect_errno()) {
   echo "<br>Failed to connect to MySQL: <br><b>" . mysqli_connect_error() . "</b>";
 }
 
+
+
+
+// Create table
+$sql="CREATE TABLE Persons(FirstName CHAR(30),LastName CHAR(30),Age INT)";
+
+// Execute query
+if (mysqli_query($con,$sql)) {
+  echo "Table persons created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($con);
+}
+
+$sql = "CREATE TABLE Persons
+(
+PID INT NOT NULL AUTO_INCREMENT,
+PRIMARY KEY(PID),
+FirstName CHAR(15),
+LastName CHAR(15),
+Age INT
+)";
 
 ?>
 
