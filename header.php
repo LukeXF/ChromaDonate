0b0069726284d1a0a61b07456bc55d9086cf03e6
LukeXF <me@luke.sx>
2014-06-24 04:22:13 +0100

    header adding

--- header.php
+++ header.php
@@ -1,1 +1,17 @@
-header.php
+<!doctype html>
+<html lang="en">
+<head>
+
+	<title>Chroma Hills - Donate</title>
+
+	<meta name="description" content="Chroma Hills donation system for fans of the Chroma Hills resource pack.">
+	<meta name="author" content="Luke Brown">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<?php require 'protected/config.php'; ?>
+</head>
+
+<body>
