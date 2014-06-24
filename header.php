32276e6d57e410d013a2260e409e7f0755a748bc
LukeXF <me@luke.sx>
2014-06-24 04:53:18 +0100

    style ordering switch

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 
-	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
 
 	<?php require 'protected/config.php'; ?>
 </head>
