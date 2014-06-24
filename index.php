ef81ab3dac206198b6e745d4ee7c163e8b8e2f9c
LukeXF <me@luke.sx>
2014-06-24 04:47:59 +0100

    added css for navbar and footer.

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php include 'header.php'; ?>
 <?php include 'navbar.php'; ?>
 
-<h1>ChromaDonate</h1>
-
     <!-- Begin page content -->
     <div class="container">
       <div class="page-header">
-        <h1>Sticky footer with fixed navbar</h1>
+        <h1>Chroma Hills Donation System</h1>
       </div>
-      <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
-      <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+      <p class="lead">Donate to one of the most popular resource packs in the whole of Minecraft to show your support and receive benefits to exlusive/early-access content.</p>
+      <p>Or <a href="http://chromahills.com">return back</a> to the main site.</p>
     </div>
 
 <?php require 'footer.php'; ?>
