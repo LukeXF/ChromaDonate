f82a43c25d328fc238a612044bec30ad85c317f2
LukeXF <me@luke.sx>
2014-06-24 04:17:15 +0100

    Added footer and header sections

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-
-	<title>Chroma Hills - Donate</title>
-	<meta name="description" content="Chroma Hills donation system for fans of the Chroma Hills resource pack.">
-	<meta name="author" content="Luke Brown">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="utf-8">
-
-	<link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
+<?php require 'header.php'; ?>
 
 <h1>ChromaDonate</h1>
 
-<?php
-echo "PHP Test!";
-?>
-
-<?php
-	require 'protected/config.php';
-?>
-
-</body>
-</html>
+<?php require 'footer.php'; ?>
--- install.php
+++ install.php
@@ -1,28 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-
-	<title>Chroma Hills - Donate</title>
-	<meta name="description" content="Chroma Hills donation system for fans of the Chroma Hills resource pack.">
-	<meta name="author" content="Luke Brown">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta charset="utf-8">
-
-	<link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
-
-<h1>ChromaDonate - Install</h1>
-
-<?php
-echo "PHP Test!";
-?>
-
-<?php
-	require 'protected/config.php';
-?>
+<?php require 'header.php'; ?>
 
 <?php
 
@@ -48,5 +24,4 @@ Age INT
 
 ?>
 
-</body>
-</html>
+<?php require 'footer.php'; ?>
