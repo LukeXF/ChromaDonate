efa32e5346d1ffc98474d143db0445efd7af3341
LukeXF <me@luke.sx>
2014-06-24 05:15:22 +0100

    Fixed assets loading correctly.

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
-<?php include 'assets/header.php'; ?>
-<?php include 'assets/navbar.php'; ?>
+<?php include './assets/header.php'; ?>
+<?php include './assets/navbar.php'; ?>
 
     <!-- Begin page content -->
     <div class="container">
       <div class="page-header">
-      	<img src="img/CHLogo.png" alt="some_text" width="1000px">
+      	<img src="./assets/img/CHLogo.png" alt="some_text" width="1000px">
         <h1>Chroma Hills Donation System</h1>
       </div>
       <p class="lead">Donate to one of the most popular resource packs in the whole of Minecraft to show your support and receive benefits to exlusive/early-access content.</p>
       <p>Or <a href="http://chromahills.com">return back</a> to the main site.</p>
     </div>
 
-<?php require 'assets/footer.php'; ?>
+<?php require './assets/footer.php'; ?>
