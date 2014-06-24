e949ddc5e36bba982767a4b97b61f8a1f4e9b12b
LukeXF <me@luke.sx>
2014-06-24 16:45:31 +0100

    Major overhaul of UI on index.

--- assets/header.php
+++ assets/header.php
@@ -9,10 +9,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="utf-8">
 
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="./assets/css/bootstrap.css">
+	<link rel="stylesheet" href="./assets/css/style.css">
 
-	<?php require 'protected/config.php'; ?>
+	<script type="text/javascript" src="//use.typekit.net/gky2akt.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+	<?php require './assets/protected/config.php'; ?>
+
 </head>
-
-<body>
--- index.php
+++ index.php
@@ -1,14 +1,165 @@
 <?php include './assets/header.php'; ?>
 <?php include './assets/navbar.php'; ?>
 
-    <!-- Begin page content -->
-    <div class="container">
-      <div class="page-header">
-      	<img src="./assets/img/CHLogo.png" alt="some_text" width="1000px">
-        <h1>Chroma Hills Donation System</h1>
-      </div>
-      <p class="lead">Donate to one of the most popular resource packs in the whole of Minecraft to show your support and receive benefits to exlusive/early-access content.</p>
-      <p>Or <a href="http://chromahills.com">return back</a> to the main site.</p>
-    </div>
+<!-- Begin page content -->
+
+
+
+
+
+
+
+
+	<div class="dark"><div class="container">
+
+		<div class="page-header">
+			<img src="./assets/img/CHLogo.png" alt="some_text" width="1000px">
+			<h1>Chroma Hills Donation System</h1>
+		</div>
+		<p class="lead width-limit">Donate to one of the most popular resource packs in the whole of Minecraft to show your support and receive benefits to exclusive/early-access content.</p>
+		<p>Or <a href="http://chromahills.com">return back</a> to the main site.</p>
+
+	</div><!--contain--></div><!--dark-->
+
+
+
+
+
+
+
+
+
+
+	<div class="cream"><div class="container">
+
+		<h1 class="hobo">Why Donate?</h1>
+
+
+		<!--========================= Benefit One ========================= -->
+		<div class="row">
+
+			<div class="col-md-2 col-md-offset-2">
+				<img src="./assets/img/benefits/1a.png" alt="some_text" height="150px">
+			</div><!--end of 4th-->
+
+			<div class="col-md-7 why-donate">
+				<h2>
+					Receive early access on all texture pack and mod releases weeks before anyone else and get your input into the design.
+				</h2>
+			</div><!--end of 8th-->
+
+		</div><!--row-->
+
+		<!--========================= Benefit Four ========================= -->
+		<div class="row">
+
+			<div class="col-md-7 why-donate col-md-offset-1">
+				<h2>
+					You will be have priority and rocket speeds to all Chroma Hills downloads above anyone else.
+				</h2>
+			</div><!--end of 8th-->
+
+			<div class="col-md-2">
+				<img src="./assets/img/benefits/4.png" alt="some_text" height="150px">
+			</div><!--end of 4th-->
+
+		</div><!--row-->
+
+		<!--========================= Benefit One ========================= -->
+		<div class="row">
+
+			<div class="col-md-2 col-md-offset-2">
+				<img src="./assets/img/benefits/1.png" alt="some_text" height="150px">
+			</div><!--end of 4th-->
+
+			<div class="col-md-7 why-donate">
+				<h2>
+					Make the developer happy and provide motivation to encourage the project to keep momentum.
+				</h2>
+			</div><!--end of 8th-->
+
+		</div><!--row-->
+
+		<!--========================= Benefit Two ========================= -->
+		<div class="row">
+
+			<div class="col-md-7 why-donate col-md-offset-1">
+				<h2>
+					Allows for a content filled texture pack completed with no gaps or holes missed. Beautiful.
+				</h2>
+			</div><!--end of 8th-->
+
+			<div class="col-md-2">
+				<img src="./assets/img/benefits/2.png" alt="some_text" height="150px">
+			</div><!--end of 4th-->
+
+		</div><!--row-->
+
+		<!--========================= Benefit Three ========================= -->
+		<div class="row">
+
+			<div class="col-md-2 col-md-offset-2">
+				<img src="./assets/img/benefits/3.png" alt="some_text" height="150px">
+			</div><!--end of 4th-->
+
+			<div class="col-md-7 why-donate">
+				<h2>
+					Sets the building blocks to future innovative projects that will benefit yourself and the community.
+				</h2>
+			</div><!--end of 8th-->
+
+		</div><!--row-->
+
+	</div><!--contain--></div><!--cream-->
+
+
+
+
+
+
+
+
+	<div class="blue"><div class="container">
+
+		<h1>Still not convinced?</h1>
+		<p class="lead width-limit">Just take a look at the vast and growing community that supports the Chroma Hills resource pack and all related projects. Get involved today!</p>
+		<p>Or <a href="http://chromahills.com">return back</a> to the main site.</p>
+
+		<img class="social" src="./assets/img/icon/twitter-128.png" alt="twitter" href="http://twitter.com/sylconesjs">
+		<img class="social" src="./assets/img/icon/reddit-128.png" alt="reddit" href="http://reddit.com/sylconesjs">
+		<img class="social" src="./assets/img/icon/facebook-128.png" alt="facebook" href="http://facebook.com/sylconesjs">
+		<img class="social" src="./assets/img/icon/email-128.png" alt="email" href="mailto:info@chromahills.com">
+		<img class="social" src="./assets/img/icon/youtube-128.png" alt="youtube" href="http://youtube.com/sylconesjs">
+
+	</div><!--contain--></div><!--blue-->
+
+
+
+
+
+
+
+
+
+
+
+
+	<div class="grey"><div class="container">
+
+		<h1>Let's Begin!</h1>
+		<p class="lead width-limit">So after reading though all this all you have to do to continue is simply enter in your name to begin, it really is that simple.</p>
+
+		<div class="browser">
+
+			<div class="bar">
+				<input class="address" value="" placeholder="Steve Smith" target="_blank" name="u" id="u">
+				<input type="submit" value="enter" class="button button-grey">
+			</div>
+
+		</div>
+
+
+	</div><!--contain--></div><!--grey-->
+
 
 <?php require './assets/footer.php'; ?>
